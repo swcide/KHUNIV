1126085b81aa2db63e23b3b58d12e34d9749daef
alexbrowncow <69133021+alexbrowncow@users.noreply.github.com>
2020-10-27 22:31:26 +0900

    2020-10-27 vo Sungho

--- src/main/java/com/kh/univ/member/model/vo/Member.java
+++ src/main/java/com/kh/univ/member/model/vo/Member.java
@@ -1,135 +1,133 @@
-package com.kh.spring.member.model.vo;
+package com.kh.univ.member.model.vo;
 
 import java.sql.Date;
 
 public class Member {
-	private String id;
-	private String pwd;
-	private String name;
-	private String email;
-	private String gender;
-	private int age;
-	private String phone;
-	private String address;
+	
+	private String semester;
+	private String sNo;
+	private String sPwd;
+	private String sName;
+	private String sEmail;
+	private String sPhone;
+	private String sSsn;
+	private String sImage;
+	private String sAddress;
 	private Date enrollDate;
-	private Date updateDate;
-	private String mStatus;
-	
-	public Member(String id, String pwd, String name, String email, String gender, int age, String phone,
-			String address, Date enrollDate, Date updateDate, String mStatus) {
-		super();
-		this.id = id;
-		this.pwd = pwd;
-		this.name = name;
-		this.email = email;
-		this.gender = gender;
-		this.age = age;
-		this.phone = phone;
-		this.address = address;
-		this.enrollDate = enrollDate;
-		this.updateDate = updateDate;
-		this.mStatus = mStatus;
-	}
-	
+	private String sStatus;
+	private String cpNo;
+	private String dNo;
 	public Member() {
 		super();
+		// TODO Auto-generated constructor stub
 	}
-
-	public String getId() {
-		return id;
+	public Member(String semester, String sNo, String sPwd, String sName, String sEmail, String sPhone, String sSsn,
+			String sImage, String sAddress, Date enrollDate, String sStatus, String cpNo, String dNo) {
+		super();
+		this.semester = semester;
+		this.sNo = sNo;
+		this.sPwd = sPwd;
+		this.sName = sName;
+		this.sEmail = sEmail;
+		this.sPhone = sPhone;
+		this.sSsn = sSsn;
+		this.sImage = sImage;
+		this.sAddress = sAddress;
+		this.enrollDate = enrollDate;
+		this.sStatus = sStatus;
+		this.cpNo = cpNo;
+		this.dNo = dNo;
 	}
-
-	public void setId(String id) {
-		this.id = id;
+	public String getSemester() {
+		return semester;
 	}
-
-	public String getPwd() {
-		return pwd;
+	public void setSemester(String semester) {
+		this.semester = semester;
 	}
-
-	public void setPwd(String pwd) {
-		this.pwd = pwd;
+	public String getsNo() {
+		return sNo;
 	}
-
-	public String getName() {
-		return name;
+	public void setsNo(String sNo) {
+		this.sNo = sNo;
 	}
-
-	public void setName(String name) {
-		this.name = name;
+	public String getsPwd() {
+		return sPwd;
 	}
-
-	public String getEmail() {
-		return email;
+	public void setsPwd(String sPwd) {
+		this.sPwd = sPwd;
 	}
-
-	public void setEmail(String email) {
-		this.email = email;
+	public String getsName() {
+		return sName;
 	}
-
-	public String getGender() {
-		return gender;
+	public void setsName(String sName) {
+		this.sName = sName;
 	}
-
-	public void setGender(String gender) {
-		this.gender = gender;
+	public String getsEmail() {
+		return sEmail;
 	}
-
-	public int getAge() {
-		return age;
+	public void setsEmail(String sEmail) {
+		this.sEmail = sEmail;
 	}
-
-	public void setAge(int age) {
-		this.age = age;
+	public String getsPhone() {
+		return sPhone;
 	}
-
-	public String getPhone() {
-		return phone;
+	public void setsPhone(String sPhone) {
+		this.sPhone = sPhone;
 	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
+	public String getsSsn() {
+		return sSsn;
 	}
-
-	public String getAddress() {
-		return address;
+	public void setsSsn(String sSsn) {
+		this.sSsn = sSsn;
 	}
-
-	public void setAddress(String address) {
-		this.address = address;
+	public String getsImage() {
+		return sImage;
 	}
-
-	public Date getenrollDate() {
+	public void setsImage(String sImage) {
+		this.sImage = sImage;
+	}
+	public String getsAddress() {
+		return sAddress;
+	}
+	public void setsAddress(String sAddress) {
+		this.sAddress = sAddress;
+	}
+	public Date getEnrollDate() {
 		return enrollDate;
 	}
-
-	public void setenrollDate(Date enrollDate) {
+	public void setEnrollDate(Date enrollDate) {
 		this.enrollDate = enrollDate;
 	}
-
-	public Date getUpdateDate() {
-		return updateDate;
+	public String getsStatus() {
+		return sStatus;
 	}
-
-	public void setUpdateDate(Date updateDate) {
-		this.updateDate = updateDate;
+	public void setsStatus(String sStatus) {
+		this.sStatus = sStatus;
 	}
-
-	public String getmStatus() {
-		return mStatus;
+	public String getCpNo() {
+		return cpNo;
 	}
-
-	public void setmStatus(String mStatus) {
-		this.mStatus = mStatus;
+	public void setCpNo(String cpNo) {
+		this.cpNo = cpNo;
 	}
-
+	public String getdNo() {
+		return dNo;
+	}
+	public void setdNo(String dNo) {
+		this.dNo = dNo;
+	}
 	@Override
 	public String toString() {
-		return "Member [id=" + id + ", pwd=" + pwd + ", name=" + name + ", email=" + email + ", gender=" + gender
-				+ ", age=" + age + ", phone=" + phone + ", address=" + address + ", enrollDate=" + enrollDate
-				+ ", updateDate=" + updateDate + ", mStatus=" + mStatus + "]";
+		return "Member [semester=" + semester + ", sNo=" + sNo + ", sPwd=" + sPwd + ", sName=" + sName + ", sEmail="
+				+ sEmail + ", sPhone=" + sPhone + ", sSsn=" + sSsn + ", sImage=" + sImage + ", sAddress=" + sAddress
+				+ ", enrollDate=" + enrollDate + ", sStatus=" + sStatus + ", cpNo=" + cpNo + ", dNo=" + dNo + "]";
 	}
 	
-}
+	
+	
+	
+	
+	
+}	
 
 
