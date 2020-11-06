376b5561a91a165cbedbef8ab0d1a0e14d4f130c
swcide <70251809+swcide@users.noreply.github.com>
2020-11-06 11:04:46 +0900

    1

--- src/main/java/com/kh/univ/notice/controller/NoticeController.java
+++ src/main/java/com/kh/univ/notice/controller/NoticeController.java
@@ -1,5 +1,15 @@
 package com.kh.univ.notice.controller;
 
-public class NoticeController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+import com.kh.univ.notice.model.service.NoticeService;
+
+@Controller
+public class NoticeController {
+	
+	@Autowired NoticeService nService
+	
+	public ModelAndView noticeDetail(ModelAndView mv, int bid,)
+	
 }
--- src/main/java/com/kh/univ/notice/model/vo/Notice.java
+++ src/main/java/com/kh/univ/notice/model/vo/Notice.java
@@ -1,5 +1,18 @@
 package com.kh.univ.notice.model.vo;
 
+import java.sql.Date;
+
 public class Notice {
+	
+	private int nId;
+	private String nTitle;
+	private String nWriter;
+	private String nContent;
+	private String originalFileName;
+	private String renameFileName;
+	private int nCount ;
+	private Date nCreateDate;
+	private Date nModifyDate;
+	private String nStatus;
 
 }
