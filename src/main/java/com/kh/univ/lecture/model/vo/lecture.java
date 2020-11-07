26dbf266d65df13186f2ad2df2063a3d9eb8fd0c
adios <adios@LAPTOP-0LR8RC3N>
2020-11-07 23:34:25 +0900

    2020-11-07 11:33 lecture vo

---
+++ src/main/java/com/kh/univ/lecture/model/vo/lecture.java
@@ -0,0 +1,43 @@
+package com.kh.univ.lecture.model.vo;
+
+import java.sql.Date;
+
+public class lecture {
+	private int lecNo;
+	private String lecName;
+	private String lecProf;
+	private String lecType;
+	private String lecSummary;
+	private String lecGoal;
+	private String lecMethod;
+	private String evalMethod;
+	private String assignment;
+	private String lecExperiment;
+	private String relatedLec;
+	private String lecTextbook;
+	private String lecPt1;
+	private String lecPt2;
+	private String lecPt3;
+	private String lecPt4;
+	private String lecPt5;
+	private String lecPt6;
+	private String lecPt7;
+	private String lecPt8;
+	private String lecPt9;
+	private String lecPt10;
+	private String lecPt11;
+	private String lecPt12;
+	private String lecPt13;
+	private String lecPt14;
+	private String lecPt15;
+	
+	
+	
+	public lecture() {
+	}
+
+
+	
+
+	
+}
