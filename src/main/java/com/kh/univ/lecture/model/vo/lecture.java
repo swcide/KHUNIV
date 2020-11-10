871d65c72895548c670eab7ce9f76874194b465d
adios <adios@LAPTOP-0LR8RC3N>
2020-11-10 12:19:31 +0900

    2020-11-10 진태수정중간커밋

--- src/main/java/com/kh/univ/lecture/model/vo/lecture.java
+++ src/main/java/com/kh/univ/lecture/model/vo/lecture.java
@@ -37,6 +37,378 @@ public class lecture {
 	}
 
 
+
+	public lecture(int lecNo, String lecName, String lecProf, String lecType, String lecSummary, String lecGoal, String lecMethod, String evalMethod,
+			String assignment, String lecExperiment, String relatedLec, String lecTextbook, String lecPt1, String lecPt2, String lecPt3,
+			String lecPt4, String lecPt5, String lecPt6, String lecPt7, String lecPt8, String lecPt9, String lecPt10, String lecPt11, String lecPt12,
+			String lecPt13, String lecPt14, String lecPt15) {
+		super();
+		this.lecNo = lecNo;
+		this.lecName = lecName;
+		this.lecProf = lecProf;
+		this.lecType = lecType;
+		this.lecSummary = lecSummary;
+		this.lecGoal = lecGoal;
+		this.lecMethod = lecMethod;
+		this.evalMethod = evalMethod;
+		this.assignment = assignment;
+		this.lecExperiment = lecExperiment;
+		this.relatedLec = relatedLec;
+		this.lecTextbook = lecTextbook;
+		this.lecPt1 = lecPt1;
+		this.lecPt2 = lecPt2;
+		this.lecPt3 = lecPt3;
+		this.lecPt4 = lecPt4;
+		this.lecPt5 = lecPt5;
+		this.lecPt6 = lecPt6;
+		this.lecPt7 = lecPt7;
+		this.lecPt8 = lecPt8;
+		this.lecPt9 = lecPt9;
+		this.lecPt10 = lecPt10;
+		this.lecPt11 = lecPt11;
+		this.lecPt12 = lecPt12;
+		this.lecPt13 = lecPt13;
+		this.lecPt14 = lecPt14;
+		this.lecPt15 = lecPt15;
+	}
+
+
+
+	public int getLecNo() {
+		return lecNo;
+	}
+
+
+
+	public void setLecNo(int lecNo) {
+		this.lecNo = lecNo;
+	}
+
+
+
+	public String getLecName() {
+		return lecName;
+	}
+
+
+
+	public void setLecName(String lecName) {
+		this.lecName = lecName;
+	}
+
+
+
+	public String getLecProf() {
+		return lecProf;
+	}
+
+
+
+	public void setLecProf(String lecProf) {
+		this.lecProf = lecProf;
+	}
+
+
+
+	public String getLecType() {
+		return lecType;
+	}
+
+
+
+	public void setLecType(String lecType) {
+		this.lecType = lecType;
+	}
+
+
+
+	public String getLecSummary() {
+		return lecSummary;
+	}
+
+
+
+	public void setLecSummary(String lecSummary) {
+		this.lecSummary = lecSummary;
+	}
+
+
+
+	public String getLecGoal() {
+		return lecGoal;
+	}
+
+
+
+	public void setLecGoal(String lecGoal) {
+		this.lecGoal = lecGoal;
+	}
+
+
+
+	public String getLecMethod() {
+		return lecMethod;
+	}
+
+
+
+	public void setLecMethod(String lecMethod) {
+		this.lecMethod = lecMethod;
+	}
+
+
+
+	public String getEvalMethod() {
+		return evalMethod;
+	}
+
+
+
+	public void setEvalMethod(String evalMethod) {
+		this.evalMethod = evalMethod;
+	}
+
+
+
+	public String getAssignment() {
+		return assignment;
+	}
+
+
+
+	public void setAssignment(String assignment) {
+		this.assignment = assignment;
+	}
+
+
+
+	public String getLecExperiment() {
+		return lecExperiment;
+	}
+
+
+
+	public void setLecExperiment(String lecExperiment) {
+		this.lecExperiment = lecExperiment;
+	}
+
+
+
+	public String getRelatedLec() {
+		return relatedLec;
+	}
+
+
+
+	public void setRelatedLec(String relatedLec) {
+		this.relatedLec = relatedLec;
+	}
+
+
+
+	public String getLecTextbook() {
+		return lecTextbook;
+	}
+
+
+
+	public void setLecTextbook(String lecTextbook) {
+		this.lecTextbook = lecTextbook;
+	}
+
+
+
+	public String getLecPt1() {
+		return lecPt1;
+	}
+
+
+
+	public void setLecPt1(String lecPt1) {
+		this.lecPt1 = lecPt1;
+	}
+
+
+
+	public String getLecPt2() {
+		return lecPt2;
+	}
+
+
+
+	public void setLecPt2(String lecPt2) {
+		this.lecPt2 = lecPt2;
+	}
+
+
+
+	public String getLecPt3() {
+		return lecPt3;
+	}
+
+
+
+	public void setLecPt3(String lecPt3) {
+		this.lecPt3 = lecPt3;
+	}
+
+
+
+	public String getLecPt4() {
+		return lecPt4;
+	}
+
+
+
+	public void setLecPt4(String lecPt4) {
+		this.lecPt4 = lecPt4;
+	}
+
+
+
+	public String getLecPt5() {
+		return lecPt5;
+	}
+
+
+
+	public void setLecPt5(String lecPt5) {
+		this.lecPt5 = lecPt5;
+	}
+
+
+
+	public String getLecPt6() {
+		return lecPt6;
+	}
+
+
+
+	public void setLecPt6(String lecPt6) {
+		this.lecPt6 = lecPt6;
+	}
+
+
+
+	public String getLecPt7() {
+		return lecPt7;
+	}
+
+
+
+	public void setLecPt7(String lecPt7) {
+		this.lecPt7 = lecPt7;
+	}
+
+
+
+	public String getLecPt8() {
+		return lecPt8;
+	}
+
+
+
+	public void setLecPt8(String lecPt8) {
+		this.lecPt8 = lecPt8;
+	}
+
+
+
+	public String getLecPt9() {
+		return lecPt9;
+	}
+
+
+
+	public void setLecPt9(String lecPt9) {
+		this.lecPt9 = lecPt9;
+	}
+
+
+
+	public String getLecPt10() {
+		return lecPt10;
+	}
+
+
+
+	public void setLecPt10(String lecPt10) {
+		this.lecPt10 = lecPt10;
+	}
+
+
+
+	public String getLecPt11() {
+		return lecPt11;
+	}
+
+
+
+	public void setLecPt11(String lecPt11) {
+		this.lecPt11 = lecPt11;
+	}
+
+
+
+	public String getLecPt12() {
+		return lecPt12;
+	}
+
+
+
+	public void setLecPt12(String lecPt12) {
+		this.lecPt12 = lecPt12;
+	}
+
+
+
+	public String getLecPt13() {
+		return lecPt13;
+	}
+
+
+
+	public void setLecPt13(String lecPt13) {
+		this.lecPt13 = lecPt13;
+	}
+
+
+
+	public String getLecPt14() {
+		return lecPt14;
+	}
+
+
+
+	public void setLecPt14(String lecPt14) {
+		this.lecPt14 = lecPt14;
+	}
+
+
+
+	public String getLecPt15() {
+		return lecPt15;
+	}
+
+
+
+	public void setLecPt15(String lecPt15) {
+		this.lecPt15 = lecPt15;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "lecture [lecNo=" + lecNo + ", lecName=" + lecName + ", lecProf=" + lecProf + ", lecType=" + lecType + ", lecSummary=" + lecSummary
+				+ ", lecGoal=" + lecGoal + ", lecMethod=" + lecMethod + ", evalMethod=" + evalMethod + ", assignment=" + assignment
+				+ ", lecExperiment=" + lecExperiment + ", relatedLec=" + relatedLec + ", lecTextbook=" + lecTextbook + ", lecPt1=" + lecPt1
+				+ ", lecPt2=" + lecPt2 + ", lecPt3=" + lecPt3 + ", lecPt4=" + lecPt4 + ", lecPt5=" + lecPt5 + ", lecPt6=" + lecPt6 + ", lecPt7="
+				+ lecPt7 + ", lecPt8=" + lecPt8 + ", lecPt9=" + lecPt9 + ", lecPt10=" + lecPt10 + ", lecPt11=" + lecPt11 + ", lecPt12=" + lecPt12
+				+ ", lecPt13=" + lecPt13 + ", lecPt14=" + lecPt14 + ", lecPt15=" + lecPt15 + "]";
+	}
+
+	
 	
 
 	
