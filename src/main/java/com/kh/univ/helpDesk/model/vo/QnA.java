e1dc46064fc7256e137b0e63f2f2974e0d7718f8
rlgns4736 <70184151+rlgns4736@users.noreply.github.com>
2020-11-11 16:22:02 +0900

    qna 진짜 다 끝

--- src/main/java/com/kh/univ/helpDesk/model/vo/QnA.java
+++ src/main/java/com/kh/univ/helpDesk/model/vo/QnA.java
@@ -1,6 +1,7 @@
 package com.kh.univ.helpDesk.model.vo;
 
 import java.sql.Date;
+import java.util.ArrayList;
 
 public class QnA {
 	private int qnaId;
@@ -12,12 +13,15 @@ public class QnA {
 	private Date qnaModifyDate;
 	private String qnaStatus;
 	private String qnaName;
+	private int rCount;
 	
-	
-	public QnA() {}
+	private ArrayList<Reply> replyList;
 
+	public QnA() {
+	}
 
-	public QnA(int qnaId, String qnaTitle, String qnaWriter, String qnaContent, int qnaCount, Date qnaCreateDate, Date qnaModifyDate, String qnaStatus, String qnaName) {
+	public QnA(int qnaId, String qnaTitle, String qnaWriter, String qnaContent, int qnaCount, Date qnaCreateDate, Date qnaModifyDate, String qnaStatus, String qnaName, int rCount,
+			ArrayList<Reply> replyList) {
 		super();
 		this.qnaId = qnaId;
 		this.qnaTitle = qnaTitle;
@@ -28,124 +32,126 @@ public class QnA {
 		this.qnaModifyDate = qnaModifyDate;
 		this.qnaStatus = qnaStatus;
 		this.qnaName = qnaName;
+		this.rCount = rCount;
+		this.replyList = replyList;
 	}
-
 
 	public int getQnaId()
 		{
 			return qnaId;
 		}
 
-
 	public void setQnaId(int qnaId)
 		{
 			this.qnaId = qnaId;
 		}
-
 
 	public String getQnaTitle()
 		{
 			return qnaTitle;
 		}
 
-
 	public void setQnaTitle(String qnaTitle)
 		{
 			this.qnaTitle = qnaTitle;
 		}
-
 
 	public String getQnaWriter()
 		{
 			return qnaWriter;
 		}
 
-
 	public void setQnaWriter(String qnaWriter)
 		{
 			this.qnaWriter = qnaWriter;
 		}
-
 
 	public String getQnaContent()
 		{
 			return qnaContent;
 		}
 
-
 	public void setQnaContent(String qnaContent)
 		{
 			this.qnaContent = qnaContent;
 		}
-
 
 	public int getQnaCount()
 		{
 			return qnaCount;
 		}
 
-
 	public void setQnaCount(int qnaCount)
 		{
 			this.qnaCount = qnaCount;
 		}
-
 
 	public Date getQnaCreateDate()
 		{
 			return qnaCreateDate;
 		}
 
-
 	public void setQnaCreateDate(Date qnaCreateDate)
 		{
 			this.qnaCreateDate = qnaCreateDate;
 		}
-
 
 	public Date getQnaModifyDate()
 		{
 			return qnaModifyDate;
 		}
 
-
 	public void setQnaModifyDate(Date qnaModifyDate)
 		{
 			this.qnaModifyDate = qnaModifyDate;
 		}
-
 
 	public String getQnaStatus()
 		{
 			return qnaStatus;
 		}
 
-
 	public void setQnaStatus(String qnaStatus)
 		{
 			this.qnaStatus = qnaStatus;
 		}
-
 
 	public String getQnaName()
 		{
 			return qnaName;
 		}
 
-
 	public void setQnaName(String qnaName)
 		{
 			this.qnaName = qnaName;
 		}
 
+	public int getrCount()
+		{
+			return rCount;
+		}
+
+	public void setrCount(int rCount)
+		{
+			this.rCount = rCount;
+		}
+
+	public ArrayList<Reply> getReplyList()
+		{
+			return replyList;
+		}
+
+	public void setReplyList(ArrayList<Reply> replyList)
+		{
+			this.replyList = replyList;
+		}
 
 	@Override
 	public String toString()
 		{
 			return "QnA [qnaId=" + qnaId + ", qnaTitle=" + qnaTitle + ", qnaWriter=" + qnaWriter + ", qnaContent=" + qnaContent + ", qnaCount=" + qnaCount + ", qnaCreateDate=" + qnaCreateDate
-					+ ", qnaModifyDate=" + qnaModifyDate + ", qnaStatus=" + qnaStatus + ", qnaName=" + qnaName + "]";
+					+ ", qnaModifyDate=" + qnaModifyDate + ", qnaStatus=" + qnaStatus + ", qnaName=" + qnaName + ", rCount=" + rCount + ", replyList=" + replyList + "]";
 		}
-
 	
 	
 }
