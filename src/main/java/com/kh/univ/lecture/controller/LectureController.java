bf5dc1b80fc8e5a57018c7051335e0e69c3a716a
young <young@alex>
2020-11-15 06:42:15 +0900

    2020-11-15 강의동 출석 작업중

--- src/main/java/com/kh/univ/lecture/controller/LectureController.java
+++ src/main/java/com/kh/univ/lecture/controller/LectureController.java
@@ -12,17 +12,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.kh.univ.common.PageInfo;
+import com.kh.univ.common.Pagination;
+import com.kh.univ.lecture.model.service.LectureService;
 import com.kh.univ.lecture.model.service.profLecService;
+import com.kh.univ.lecture.model.vo.Attendance;
 import com.kh.univ.lecture.model.vo.LectureClass;
 import com.kh.univ.lecture.model.vo.LectureList;
 import com.kh.univ.lecture.model.vo.LecturePlan;
 import com.kh.univ.lecture.model.vo.LecturePlanWeek;
+import com.kh.univ.univlife.board.human.model.vo.HumanBoard;
 
 @Controller //이거 안쓰면 에러남 
 public class LectureController {
 	
 	@Autowired
-	profLecService plService;
+	LectureService lService;
 	
 	@RequestMapping(value = "lecturelist.do")
 	public String signup( Model model) {
@@ -31,27 +36,29 @@ public class LectureController {
 	}
 	
 	/**
-	 * Simply selects the home view to render by returning its name.
+	 * 1.출석현황다떼
+	 * 
+	 * @param mv
+	 * @param currentPage
+	 * @return
 	 */
 	@RequestMapping(value = "attendance.do")
-	public String attendance( Model model) {
+	public ModelAndView humanBoardList(ModelAndView mv,
+			@RequestParam(value = "currentPage", required = false, defaultValue = "1") int currentPage) {
+
+		int listCount = lService.getListCount();
+
+		PageInfo pi = Pagination.getPageInfo(currentPage, listCount);
 		
-		return "ad_lecture/ad_attendance";
-	}
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "grade.do")
-	public String grade(Model model) {
 		
-		return "ad_lecture/ad_grade";
+
+		ArrayList<Attendance> list = lService.selectList(pi);
+
+		mv.addObject("list", list);
+		mv.addObject("pi", pi);
+		mv.setViewName("ad_lecture/ad_attendance");
+		return mv;
 	}
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	
 	
 	
 	/**
@@ -64,33 +71,4 @@ public class LectureController {
 	}
 	
 	
-	@RequestMapping(value = "ad_lect_list.do")
-	public ModelAndView ad_Lecture_List(ModelAndView mv, HttpSession session ) {
-			ArrayList<LectureList> aLc = plService.selectList(session);
-			System.out.println(aLc);
-			if(aLc != null) {
-			mv.addObject("aLc",aLc);
-			mv.setViewName("ad_register/ad_Lecture_List");
-			}else {
-				mv.addObject("msg","로그인 실패");
-				mv.setViewName("common/errorPage");
-			}
-			return mv;
-	}
-	
-	
-	@RequestMapping(value = "ad_syllabus.do")
-	public ModelAndView ad_syllabus(ModelAndView mv, @RequestParam(value="classNo")String classNo) {
-			ArrayList <LecturePlanWeek> lpw = plService.selectSyllaOne2(classNo);
-			LecturePlan lp = plService.selectSyllaOne1(classNo);
-			if(lpw != null && lp !=null) {
-			mv.addObject("lp",lp);
-			mv.addObject("lpw",lpw);
-			mv.setViewName("ad_register/ad_Syllabus_Lecture");
-			}else {
-				mv.addObject("msg","로그인 실패");
-				mv.setViewName("common/errorPage");
-			}
-			return mv;
-	}
 }
