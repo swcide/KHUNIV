ea3f159604e86a8a0096f53ef77cd366e7f6eed8
alexbrowncow <69133021+alexbrowncow@users.noreply.github.com>
2020-11-19 22:29:26 +0900

    2020-11-19 출석률

--- src/main/java/com/kh/univ/lecture/model/vo/Attendance.java
+++ src/main/java/com/kh/univ/lecture/model/vo/Attendance.java
@@ -4,21 +4,21 @@ import java.sql.Date;
 
 public class Attendance {
 	
-	private String classNo;					// 과목번호					TB_ATTENDANCE
-	protected String classType; 		    // 이수구분 					tb-class
-	protected String className; 		    // 과목명 					tb-class
-	protected String sNo; 		    	    // 학생번호					TB_ATTENDANCE
+	private String classNo;					// 과목번호				TB_ATTENDANCE
+	protected String classType; 		    // 이수구분 				TB_CLASS
+	protected String className; 		    // 과목명 					TB_CLASS
+	protected String sNo; 		    	    // 학생번호				TB_ATTENDANCE
 	protected int attendRate; 				// 출석률 					TB_ATTENDANCE
-	private Date attendance;				// 출석일자
-	private int assignment;					// 과제제출수					TB_ATTENDANCE
-	private int week;						// 주차
+	private Date attendance;				// 출석일자				TB_ATTENDANCE
+	private Date startDate;					// 개강일자				TB_CLASS
+	private int assignment;					// 과제제출수				TB_ATTENDANCE
+	private int week;						// 주차					TB_ATTENDANCE
 	
 	public Attendance() {
 	}
 
-
 	public Attendance(String classNo, String classType, String className, String sNo, int attendRate, Date attendance,
-			int assignment, int week) {
+			Date startDate, int assignment, int week) {
 		super();
 		this.classNo = classNo;
 		this.classType = classType;
@@ -26,30 +26,18 @@ public class Attendance {
 		this.sNo = sNo;
 		this.attendRate = attendRate;
 		this.attendance = attendance;
+		this.startDate = startDate;
 		this.assignment = assignment;
 		this.week = week;
 	}
 
-
-	public Date getAttendance() {
-		return attendance;
+	public String getClassNo() {
+		return classNo;
 	}
 
-
-	public void setAttendance(Date attendance) {
-		this.attendance = attendance;
+	public void setClassNo(String classNo) {
+		this.classNo = classNo;
 	}
-
-
-	public int getWeek() {
-		return week;
-	}
-
-
-	public void setWeek(int week) {
-		this.week = week;
-	}
-
 
 	public String getClassType() {
 		return classType;
@@ -83,12 +71,20 @@ public class Attendance {
 		this.attendRate = attendRate;
 	}
 
-	public String getClassNo() {
-		return classNo;
+	public Date getAttendance() {
+		return attendance;
 	}
 
-	public void setClassNo(String classNo) {
-		this.classNo = classNo;
+	public void setAttendance(Date attendance) {
+		this.attendance = attendance;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
 	}
 
 	public int getAssignment() {
@@ -99,18 +95,21 @@ public class Attendance {
 		this.assignment = assignment;
 	}
 
+	public int getWeek() {
+		return week;
+	}
+
+	public void setWeek(int week) {
+		this.week = week;
+	}
 
 	@Override
 	public String toString() {
 		return "Attendance [classNo=" + classNo + ", classType=" + classType + ", className=" + className + ", sNo="
-				+ sNo + ", attendRate=" + attendRate + ", attendance=" + attendance + ", assignment=" + assignment
-				+ ", week=" + week + "]";
+				+ sNo + ", attendRate=" + attendRate + ", attendance=" + attendance + ", startDate=" + startDate
+				+ ", assignment=" + assignment + ", week=" + week + "]";
 	}
 
+	
 
-	
-	
-	
-	
-	
 }
