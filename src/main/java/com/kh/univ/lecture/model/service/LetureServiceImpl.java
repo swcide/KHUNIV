4b6cbb7dbc831c23876114ccf90d785961895712
young <young@alex>
2020-11-22 23:13:31 +0900

    2020-11-22 내강의목록

--- src/main/java/com/kh/univ/lecture/model/service/LetureServiceImpl.java
+++ src/main/java/com/kh/univ/lecture/model/service/LetureServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import com.kh.univ.common.PageInfo;
 import com.kh.univ.lecture.model.dao.LectureDao;
 import com.kh.univ.lecture.model.vo.Attendance;
+import com.kh.univ.lecture.model.vo.LectureList;
 @Service("lService")
 public class LetureServiceImpl implements LectureService {
 	
@@ -17,6 +18,12 @@ public class LetureServiceImpl implements LectureService {
 	@Override
 	public ArrayList<Attendance> selectList(String sNo) {
 		return lDao.selectList(sNo);
+	}
+
+	@Override
+	public ArrayList<LectureList> selectList1(String sNo) {
+
+		return lDao.selectList1(sNo);
 	}
 
 
